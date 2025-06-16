3fc3e9c5b58fe85d844bdfd00a78b6cf749679c4
Your Name <you@example.com>
2025-06-16 17:51:20 +0300

    add user class

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,1 +1,14 @@
+import datetime
 
+
+class User:
+
+    def __init__(self, id: str, first_name: str, last_name: str, email: str, created_at: datetime, updated_at: datetime, is_admin: bool = False):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.is_admin = is_admin
+        
