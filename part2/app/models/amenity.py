d87f45e7ca84e7973c9765528710649b2364713a
Your Name <you@example.com>
2025-06-16 17:54:08 +0300

    add amenity class

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,1 +1,9 @@
+import datetime
 
+
+class Amenity:
+    def __init__(self, id: str, name: str, created_at: datetime.datetime, updated_at: datetime.datetime):
+        self.id = id
+        self.name = name
+        self.created_at = created_at
+        self.updated_at = updated_at
