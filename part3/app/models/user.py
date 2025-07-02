d61719af5a41a3bb992bff1addeae4ec7c5152f6
osamaghamdi <197741415+osamaghamdi@users.noreply.github.com>
2025-07-02 11:44:08 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -22,10 +22,10 @@ class User(BaseModel):
         self.is_admin = is_admin
         self.hash_password(password)
 
-  def hash_password(self, password):
-    """Hashes the password before storing it."""
-    self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-def verify_password(self, password):
-    """Verifies if the provided password matches the hashed password."""
-    return bcrypt.check_password_hash(self.password, password)
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
