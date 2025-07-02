eef64c1ef23909d9c0b07e75f4b423409a4f791b
osamaghamdi <197741415+osamaghamdi@users.noreply.github.com>
2025-07-02 11:24:59 +0300

    Create review.py

---
+++ part3/app/models/review.py
@@ -0,0 +1,16 @@
+from part3.app.models.basemodel import BaseModel
+
+
+class Review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+
+        if not text:
+            raise ValueError("Review text is required.")
+        if not (1 <= rating <= 5):
+            raise ValueError("Rating must be between 1 and 5.")
+
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
